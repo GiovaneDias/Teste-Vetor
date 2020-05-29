ad73bf5b2999c515725f50f08c58e15ba98f7eea
Giovane Dias <61985184+GioDiasDev@users.noreply.github.com>
2020-05-29 11:55:48 -0300

    Descrição como funciona o código(no arquivo .java)

--- ExercicioJava/src/exercicio.java
+++ ExercicioJava/src/exercicio.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+//Algorítimo para testas o índice do array(vetor), temos um array(vetor) de 15 números, é ultilizado um FOR para pedir que o usuário digitar um número 15 vezes, se o número digitado for igual a 30 ele mostra o índice do array(vetor), lembrando que nesse caso o índice vai de 0 a 14.
+
 public class exercicio {
     public static void main(String[] args){
         int num[]= new int[15];
